8ecbef9b9172310b1c181ba152b9706fe4fa4e85
Urs Schmidt <ufogreeks@gmail.com>
2025-07-29 09:34:15 +0200

    chores: more formatting

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 `sudo dnf install libX11-devel libXext-devel libXrender-devel libbsd-devel`
 
 ### In minilibx_linux/Makefile.mk:4
+The default compiler also needs to be changed in order to match the
+prototyping requirement of school computers.
 change to `CC	= clang`
 
 your own files should also be compiled using clang
--- src/render/render.h
+++ src/render/render.h
@@ -11,20 +11,20 @@
 /* ************************************************************************** */
 
 #ifndef RENDER_H
-#define RENDER_H
+# define RENDER_H
 
-#include "../includes/CONSTANTS.h"
-#include "../includes/structs.h"
+# include "../includes/CONSTANTS.h"
+# include "../includes/structs.h"
 
 // render
-int render_frames(t_game *game);
-int render_loop_wrapper(void);
+int				render_frames(t_game *game);
+int				render_loop_wrapper(void);
 
 // create_graphics
-void		 pixel_put(t_img *img, int x, int y, int color);
-unsigned int get_grad_col(int val, int max,
-						  unsigned int c_min, unsigned int c_max);
-void		 reset_img(t_img *img, int width, int height);
-void		 gen_noise(t_img *p);
+void			pixel_put(t_img *img, int x, int y, int color);
+unsigned int	get_grad_col(int val, int max,
+					unsigned int c_min, unsigned int c_max);
+void			reset_img(t_img *img, int width, int height);
+void			gen_noise(t_img *p);
 
 #endif
