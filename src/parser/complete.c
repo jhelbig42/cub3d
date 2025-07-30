900f97df8898c4231726268e2f0339b26a4041ae
jhelbig42 <jhelbig@student.42berlin.de>
2025-07-30 13:58:54 +0200

    fix: valgrind kind of happy;

--- src/parser/complete.c
+++ src/parser/complete.c
@@ -37,51 +37,58 @@ void    set_start_pos(t_game *game, char pos)
         game->player.plane_y = -0.66;
     }
 }
+
+bool    read_map_char(t_game *game, char **map, int *i, int *width, char *pos)
+{
+    char    cur;
+    int     j;
+
+    while (map[*i])
+    {
+        j = 0;
+        while (map[*i][j])
+        {
+            cur = map[*i][j];
+            if (cur != '0' && cur != '1' && cur != 'N'
+                && cur != 'S' && cur != 'E' && cur != 'W' && cur != ' ' && cur != '\n')
+                return (print_error("undefined symbols in map"), false);
+            if (cur == 'N' || cur == 'S' || cur == 'E' || cur == 'W')
+            {
+                if (*pos != 'D')
+                    return (print_error("more that 1 player given in map"), false);
+                *pos = cur;
+                game->player.pos_x = *i;
+                game->player.pos_y = j;
+            }
+            if (j > *width)
+                *width = j;
+            j++;
+        }
+        *i = *i + 1;
+    }
+    return (true);
+}
 //are symbols in map that are not allowed?
 //- in der map nur 0, 1 und genau 1 N S E W
 //map surrounded by walls
 bool map_str_arr_valid(t_game *game, char **map)
 {
     int     i;
-    int     j;
-    char    cur;
     char    pos;
     int     width;
 
     pos = 'D';
     i = 0;
     width = 0;
-    while (map[i])
-    {
-        j = 0;
-        while (map[i][j])
-        {
-            cur = map[i][j];
-            if (cur != '0' && cur != '1' && cur != 'N'
-                && cur != 'S' && cur != 'E' && cur != 'W' && cur != ' ' && cur != '\n')
-                return (print_error("undefined symbols in map"), false);
-            if (cur == 'N' || cur == 'S' || cur == 'E' || cur == 'W')
-            {
-                if (pos != 'D')
-                    return (print_error("more that 1 player given in map"), false);
-                pos = cur;
-                game->player.pos_x = i;
-                game->player.pos_y = j;
-            }
-            //find max_len
-            if (j > width)
-                width = j;
-            j++;
-        }
-        i++;
-    }
-    //i ist hier die Zahl der Zeilen
+    
+    read_map_char(game, map, &i, &width, &pos);
+
     game->map.lines = i;
     game->map.col = width;
     if (pos == 'D')
         return (print_error("no player given in map"), false);
     set_start_pos(game, pos);
     //flood fill here
-    //if successfull, copy into int **
+    //return floodfill false
     return (true);
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -56,23 +56,14 @@ bool	parse_map(t_game *game, char *map_name)
 	int		i;
 
 	fill_default_game(game);
-	//correct name
 	if (!correct_file_type(map_name))
 		return (false);
-    //open file
 	fd = open(map_name, O_RDONLY);
 	if (fd < 0)
 		return (print_error("could not open map file"), false);
-    /* - leerzeilen und spaces ueberspringen
-        - nach keywords suchen NO SO WE EA F C 
-        --> fill information into game struct
-        wenn andere Zeichen als type identifier: return false
-        - F C Zahlen muessen zwischen 0 und 255 liegen
-    */
-	line = get_next_line(fd);
+   	line = get_next_line(fd);
 	while (line)
 	{
-        //printf("line read: %s", line);
 		if (line[0] == 'F' || line[0] == 'C') 
 		{
 			if (!find_colors(game, line, line[0]))
@@ -83,20 +74,17 @@ bool	parse_map(t_game *game, char *map_name)
 			if (!set_wall_paths(game, line))
 				return (free(line), close(fd), false);
 		}
-		//newlines ueberspringen, sonst raus
 		else if (line[0] != '\n')
 			break ;
 		free(line);
 		line = get_next_line(fd);
 	}
-	//printf("line: %s", line);
-	// hier muessen dann alle Daten bis auf die map ausgelesen sein
-    // testen ob noch default Werte, wenn ja, dann return false
 	if (!data_complete(game))
 		return(free(line), close(fd), false);
 	map_str_arr = (char **)malloc(sizeof(char *) * 200); //max 200lines read
 	if (!map_str_arr)
 		return (print_error("error malloc map"), free(line), close(fd), false);
+	//inititalize map strings with NULL
 	i = 0;
     while (line)
 	{
@@ -106,7 +94,7 @@ bool	parse_map(t_game *game, char *map_name)
 	}
 	if (!map_str_arr_valid(game, map_str_arr))
 		return (free_str_arr(map_str_arr), close (fd), false);
+	free_str_arr(map_str_arr);
 	close(fd);
-	
 	return (true);
 }
