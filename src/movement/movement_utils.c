99e7d75f1383340f1752002c122964d6b18dd2ca
Julia Helbig <jhelbig@c4b4c1.42berlin.de>
2025-08-27 10:54:38 +0200

    fix: keeps distance to walls, door there again

--- src/movement/movement_utils.c
+++ src/movement/movement_utils.c
@@ -6,7 +6,7 @@
 /*   By: jhelbig <jhelbig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 15:13:25 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/08/27 10:19:12 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/08/27 10:47:18 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 bool	empty_field(t_map map, t_vector_d pos)
 {
-	if (map.map[(int)pos.y][(int)pos.x] == 1
-		|| map.map[(int)pos.y][(int)pos.x] == 2)
+	if (map.map[(int)(pos.y + 0.05)][(int)(pos.x + 0.05)] == 1
+		|| map.map[(int)(pos.y - 0.05)][(int)(pos.x + 0.05)] == 1
+		|| map.map[(int)(pos.y + 0.05)][(int)(pos.x - 0.05)] == 1
+		|| map.map[(int)(pos.y - 0.05)][(int)(pos.x - 0.05)] == 1
+		|| map.map[(int)(pos.y + 0.05)][(int)(pos.x + 0.05)] == 2
+		|| map.map[(int)(pos.y - 0.05)][(int)(pos.x + 0.05)] == 2
+		|| map.map[(int)(pos.y + 0.05)][(int)(pos.x - 0.05)] == 2
+		|| map.map[(int)(pos.y - 0.05)][(int)(pos.x - 0.05)] == 2)
 		return (false);
 	return (true);
 }
